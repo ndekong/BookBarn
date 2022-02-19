c45107352c00e2a58d248bd0c28f36eff790c04e
ndekong <ndiuwembassey@gmail.com>
2022-02-19 23:01:36 +0100

    BookBarn First Commit
    New books added and bug fixed

--- app/src/main/java/com/example/bookbarn/HomeActivity.kt
+++ app/src/main/java/com/example/bookbarn/HomeActivity.kt
@@ -55,6 +55,49 @@ class HomeActivity: AppCompatActivity() , BookListener {
         )
         bookList.add(book2)
 
+        val book3 = Books(
+
+            R.drawable.ftysog,
+            "Fifty Shades Of Grey",
+            "EL James",
+            "When literature student Anastasia Steele goes to interview young entrepreneur Christian Grey, she encounters a man who is beautiful, brilliant, and intimidating. The unworldly, innocent Ana is startled to realize she wants this man and, despite his enigmatic reserve, finds she is desperate to get close to him. Unable to resist Ana's quiet beauty, wit, and independent spirit, Grey admits he wants her, too--but on his own terms. Shocked yet thrilled by Grey's singular erotic tastes, Ana hesitates. For all the trappings of success--his multinational businesses, his vast wealth, his loving family--Grey is a man tormented by demons and consumed by the need to control. When the couple embarks on a daring, passionately physical affair, Ana discovers Christian Grey's secrets and explores her own dark desires."
+        )
+        bookList.add(book3)
+
+        val book4 = Books(
+
+            R.drawable.ftysdkr,
+            " Fifty Shades Darker",
+            "EL James",
+            "Daunted by the singular tastes and dark secrets of the beautiful, tormented young entrepreneur Christian Grey, Anastasia Steele has broken off their relationship to start a new career with a Seattle publishing house.\n" +
+                    "\n" +
+                    "But desire for Christian still dominates her every waking thought, and when he proposes a new arrangement, Anastasia cannot resist. They rekindle their searing sensual affair, and Anastasia learns more about the harrowing past of her damaged, driven and demanding Fifty Shades.\n" +
+                    "\n" +
+                    "While Christian wrestles with his inner demons, Anastasia must confront the anger and envy of the women who came before her, and make the most important decision of her life."
+        )
+        bookList.add(book4)
+
+        val book5 = Books(
+
+            R.drawable.ftysfreed,
+            "Fifty Shades Freed",
+            "EL James",
+            "When unworldly student Ana Steele first encountered the driven, damaged young entrepreneur Christian Grey, it sparked a sensual affair that changed both their lives irrevocably. Shocked, intrigued, and ultimately repelled by Christian's singular sexual tastes, Ana demanded a deeper commitment; determined to keep her, Christian agreed. Now, together, they have more-love, passion, intimacy, wealth, and a world of infinite possibilities. But Ana always knew that loving her Fifty Shades would not be easy and being together poses challenges neither of them ever anticipated. Ana must somehow learn to share Christian's opulent lifestyle without sacrificing her own integrity, identity, or independence; Christian must somehow overcome his compulsion to control and lay to rest the horrors that blighted his past and haunt his present. Just when it seems that together their love can conquer any obstacle, tragedy, malice and fate combine to make Ana's worst nightmares come true. Alone and desperate, she must face down the poisoned legacy of Christian's past. Seductive, shocking, sad, and funny, Fifty Shades Freed is the compelling final volume in the Fifty Shades trilogy."
+        )
+        bookList.add(book5)
+
+        val book6 = Books(
+            R.drawable.tmyd,
+            "Tell Me Your Dreams",
+            "Sidney Sheldon",
+            "The fast-paced novel from the internationally bestselling author of The Best Laid Plans, Morning, Noon & Night and Bloodline.\n" +
+                    "\n" +
+                    "Someone was following her. She had read about stalkers, but they belonged in a different, faraway world. She had no idea who it could be, who would want to harm her. She was trying desperately hard not to panic, but lately her sleep had been filled with nightmares, and she had awakened each morning with a feeling of impending doom.\n" +
+                    "\n" +
+                    "Thus begins Sidney Sheldon’s chilling novel, Tell Me Your Dreams. Three beautiful young women are suspected of committing a series of brutal murders. The police make an arrest that leads to one of the most bizarre murder trials of the century. Based on actual events, Sheldon’s novel races from London to Rome to Quebec City to San Francisco, with a climax that will leave the reader stunned."
+        )
+        bookList.add(book6)
+
 
     }
 
